6667df082b6ff6ad1de52fced04c38ea7cfd0a05
TK <infichallen@naver.com>
2023-05-08 04:05:48 +0900

    first commit

---
+++ 명품자바_4장/src/Book.java
@@ -0,0 +1,23 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Book {
+    String title;
+    String author;
+
+    public Book(String t){
+        title =t;
+        author = "작자미상";
+    }
+
+    public Book(String t, String a){
+        title = t;
+        author = a;
+    }
+
+    public static void main(String[] args){
+        Book littlePrince = new Book("어린왕자", "생텍쥐페리");
+
+        Book loveStroy = new Book("춘향전");
+        System.out.println(littlePrince.title + " " + little);
+    }
+}
---
+++ 명품자바_4장/src/RectApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Rectangle{
+    int width;
+    int height;
+
+    public int getArea(){
+        return width*height;
+    }
+}
+
+public class RectApp{
+    public static void main(String[] args){
+        Rectangle rect = new Rectangle();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(">>");
+
+    }
+}
